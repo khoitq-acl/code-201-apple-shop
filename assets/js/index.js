0cc46e2bbafbb0165c95bd348ea8518fc8505e03
khoitq-acl <khoitq@americancodelab.com>
2023-04-12 19:04:27 +0700

    Update

---
+++ assets/js/index.js
@@ -0,0 +1,96 @@
+/* 
+Hello World
+
+1. console.log function
+  syntax: console.log( value1, value2, ...)
+    
+2. string data type
+  syntax: "string value"
+
+*/
+
+console.log("Hello World");
+
+
+/*
+Data Type
+1. string                // "Hello World"
+2. number                // 123
+3. boolean               // true / false
+4. undefined             
+5. null                  
+6. object        
+7. array                 // ["Hello World", 123, true, false]
+8. symbol
+*/
+
+
+/*
+variables                // A store data method
+  syntax: 
+      var variableName = value;
+      let variableName = value;
+      const variableName = value;
+
+  different:
+    - var: functional scope, changeable value  (not recommended to use)
+    - let: block scope, changeable value
+    - const: block scope, unchangeable value
+  
+*/
+
+// let name = "Khoi";
+// const age = 25;
+
+// console.log(name, age);
+
+
+
+/*
+Condition: logical expression that evaluates to true or false
+  - Boolean: true / false
+  - Comparison: Use comparison operators like
+    == equal to
+    != not equal to
+    > greater than
+    < less than
+    >= greater than or equal to
+    <= less than or equal to
+  - Logical
+    && and
+    || or
+    ! not
+
+*/
+
+
+
+/*
+If - Else condition statement
+  syntax: 
+        if (condition) {
+          // Code to be executed if condition is true
+        } else {
+          // Code to be executed if condition is false
+        }   
+
+*/
+
+const age = 25;
+
+if (age >= 18) {
+
+  console.log("You are an adult");
+
+} else {
+
+  console.log("You are not an adult");
+
+}
+
+
+/*
+Next step
+*/
+
+
